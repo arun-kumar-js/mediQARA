c94cd868c014a4a00550d831aa96011334be68ab
arun-kumar-js <srivar63@outlook.com>
2025-08-31 22:57:28 +0530

    n

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/mediQARA/", // For custom domain, keep '/' ; for project pages use '/repo-name/'
+  base: "/", // For custom domain, keep '/' ; for project pages use '/repo-name/'
 });
